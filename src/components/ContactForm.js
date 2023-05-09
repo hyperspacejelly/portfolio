74a34aa9baed58d05342df60cee156b7aa2c662a
Lucien Jély <lucien.jely@gmail.com>
2023-05-09 17:05:07 +0200

    Contact form basic style + data mgmt

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,43 +1,94 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import axios, { Axios } from "axios";
 import './css/contactForm.css';
 
 function ContactForm({toggle, toggleOff, lang}){
-    const form = useRef(null);
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+    const [email, setEmail] = useState("");
+    const [tel, setTel] = useState("");
+    const [msg, setMsg] = useState("");
 
+    function clearForm(){
+        setFname("");
+        setLname("");
+        setEmail("");
+        setTel("");
+        setMsg("");
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const payload = {
+            fname : fname,
+            lname: lname,
+            email: email,
+            tel: tel,
+            msg: msg
+        }
+        console.log(payload);
+        clearForm();
+    }
     return(
-        <div id="contactForm" className={toggle?"contactForm-opened":"contactForm-unopened"} 
-                ref={form} 
-                onClick={()=>toggleOff()}>
+        <>
+            <div  id="contactForm" 
+                    className={toggle?"contactForm-opened":"contactForm-unopened"} 
+                    onClick={()=>toggleOff()}>
 
-            <div className="form-cont" onClick={(e)=>{e.stopPropagation();}}>
-                <h2>{lang==="fr" ? "formulaire de contact" : "contact form"}</h2>
-                <button onClick={()=>toggleOff()}>close</button>
+                <div className="form-cont" onClick={(e)=>{e.stopPropagation();}}>
+                    <h2>{lang==="fr" ? "formulaire de contact" : "contact form"}</h2>
+                    <button className="close-btn" onClick={()=>toggleOff()}>X</button>
 
-                <form className="contact-form">
-                    <section className="form-row">
-                        <label htmlFor="form-fname">{lang==="en"?"First name":"Prénom"}</label>
-                        <input type="text" name="fname" id="form-fname" placeholder={lang==="en"?"Required":"Requis"} />
+                    <form autoComplete="off" className="contact-form">
+                        <section className="form-row">
+                            <div className="form-elem">
+                                <label htmlFor="form-fname">{lang==="en"?"First name":"Prénom"}</label>
+                                <input type="text" name="fname" id="form-fname" 
+                                        placeholder={lang==="en"?"Required":"Requis"}
+                                        value={fname}
+                                        onInput={(e)=>setFname(e.target.value)} />
+                            </div>
+                            <div className="form-elem">
+                                <label htmlFor="form-lname">{lang==="en"?"Last name":"Nom"}</label>
+                                <input type="text" name="lname" id="form-lname" 
+                                    placeholder={lang==="en"?"Required":"Requis"}
+                                    value={lname}
+                                    onInput={(e)=>setLname(e.target.value)} />
+                            </div>
+                        </section>
 
-                        <label htmlFor="form-lname">{lang==="en"?"Last name":"Nom"}</label>
-                        <input type="text" name="lname" id="form-lname" placeholder={lang==="en"?"Required":"Requis"}/>
-                    </section>
-
-
-                    <section className="form-row">
-                        <label htmlFor="form-tel">{lang==="en"?"Phone number":"N° de télephone"}</label>
-                        <input type="tel" name="tel" id="form-tel"/>
-
-                        <label htmlFor="form-email">{lang==="en"?"Email address":"Adresse email"}</label>
-                        <input type="email" name="email" id="form-email"/>
-                    </section>
-        
-                    <label htmlFor="form-message">{lang==="en"?"Your message":"Votre message"}</label>
-                    <textarea id="form-message"></textarea>
-
-                    <button type="submit">{lang==="en"?"Send message": "Envoyer le message"}</button>
-                </form>
+                        <section className="form-row">
+                            <div className="form-elem">
+                                <label htmlFor="form-tel">{lang==="en"?"Phone number":"N° de télephone"}</label>
+                                <input type="tel" name="tel" id="form-tel"
+                                    value={tel}
+                                    onInput={(e)=>setTel(e.target.value)} />
+                            </div>
+                            <div className="form-elem">
+                                <label htmlFor="form-email">{lang==="en"?"Email address":"Adresse email"}</label>
+                                <input type="email" name="email" id="form-email"
+                                       value={email}
+                                       onInput={(e)=>setEmail(e.target.value)} />
+                            </div>
+                        </section>
+                        <section className="form-row row-label">
+                            <label htmlFor="form-message">{lang==="en"?"Your message":"Votre message"}</label>
+                        </section>
+                        <section className="form-row">
+                            <div className="form-elem form-msg">
+                                <textarea id="form-message" placeholder={lang==="en"?"Required":"Requis"}
+                                        value={msg}
+                                        onInput={(e)=>setMsg(e.target.value)}></textarea>
+                            </div>
+                            <div className="form-elem form-submit">
+                                <button type="submit" onClick={handleSubmit}>{lang==="en"?"Send": "Envoyer"}</button>
+                            </div>
+                       </section>
+                    </form>
+                </div>
             </div>
-        </div>
+            <div id="contactFormBG" className={toggle?"contactForm-opened":"contactForm-unopened"}></div>
+        </>
     );
 }
 
