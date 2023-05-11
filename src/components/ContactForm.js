82d93a2e44be90112cf6e6e99cdc43647862a07f
Lucien Jély <lucien.jely@gmail.com>
2023-05-11 18:18:19 +0200

    Client Side Form Validation
    + style update on contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './css/contactForm.css';
 
 function ContactForm({toggle, toggleOff, lang}){
@@ -9,6 +9,14 @@ function ContactForm({toggle, toggleOff, lang}){
     const [subject, setSubject] = useState("");
     const [msg, setMsg] = useState("");
 
+    const headerRef = useRef();
+
+    const fnameRef = useRef();
+    const lnameRef = useRef();
+    const emailRef = useRef();
+    const subjectRef = useRef();
+    const msgRef = useRef();
+
     function clearForm(){
         setFname("");
         setLname("");
@@ -16,6 +24,54 @@ function ContactForm({toggle, toggleOff, lang}){
         setTel("");
         setSubject("");
         setMsg("");
+    }
+
+    function clearErrors(){
+        fnameRef.current.className = "";
+        lnameRef.current.className = "";
+        emailRef.current.className = "";
+        subjectRef.current.className = "";
+        msgRef.current.className = "";
+    }
+
+    useEffect(()=>{
+        if(toggle===false){
+            clearErrors();
+        }
+    },[toggle]);
+
+    function validateData(payload){
+        //First we check if the required fields are empty. If they are we affect their display
+        fnameRef.current.className = payload.fname==="" ? "input-error":"";
+        lnameRef.current.className = payload.lname==="" ? "input-error":"";
+        emailRef.current.className = payload.email==="" ? "input-error":"";
+        subjectRef.current.className = payload.subject==="" ? "input-error":"";
+        msgRef.current.className = payload.msg==="" ? "input-error":"";
+
+
+
+        //then we check if the email seems like a real email
+        const emailAtIndex = payload.email.indexOf("@"); 
+        const emailDomainIndex = payload.email.lastIndexOf(".");
+
+        //if no @ or no text before @ || last . before @ || no text after last .
+        if(emailAtIndex < 1 || emailDomainIndex < emailAtIndex || emailDomainIndex > (payload.email.length - 3)){
+            emailRef.current.className = "input-error";
+            return false;
+        }
+
+       
+        let validate = true;
+
+        // if email is ok we still check if one of those fields is empty so we can return that validation failed
+        for(const[elem, value] of Object.entries(payload)){
+            if(elem != "tel" && value === ""){
+                validate=false;
+                break;
+            }
+        }
+
+        return validate;
     }
 
     async function postFormData(payload){
@@ -35,20 +91,35 @@ function ContactForm({toggle, toggleOff, lang}){
 
     function handleSubmit(e){
         e.preventDefault();
-        const payload = {
-            fname : fname,
-            lname: lname,
-            email: email,
-            tel: tel,
-            subject: subject,
-            msg: msg
+        const payload={
+            fname : fname.trim(),
+            lname: lname.trim(),
+            email: email.trim(),
+            tel: tel.trim(),
+            subject: subject.trim(),
+            msg: msg.trim()
         }
-        postFormData(payload).then(res=>{
-            console.log(res);
-            if(res.status=="200"){
-                clearForm();
-            } 
-        });
+
+        if(validateData(payload)){
+            postFormData(payload).then(res=>{
+                console.log(res.status);
+                if(res.status=="200"){
+                    document.getElementById("submitBtn").disabled = true;
+                    clearForm();
+                    headerRef.current.innerHTML = (lang==="fr")?"Message envoyé !" : "Message sent!";
+                    setTimeout(()=>{
+                        toggleOff();
+                        headerRef.current.innerHTML= (lang==="fr")?"Formulaire de contact" :"Contact form";
+                        document.getElementById("submitBtn").disabled = false;
+                    }, 3000);
+                }else{
+                    headerRef.current.innerHTML = (lang==="fr")?"Erreur d'envoi" : "Sending error";
+                    setTimeout(()=>{
+                        headerRef.current.innerHTML= (lang==="fr")?"Formulaire de contact" :"Contact form"
+                    }, 3000); 
+                } 
+            });
+        }
     }
     return(
         <>
@@ -57,7 +128,7 @@ function ContactForm({toggle, toggleOff, lang}){
                     onClick={()=>toggleOff()}>
 
                 <div className="form-cont" onClick={(e)=>{e.stopPropagation();}}>
-                    <h2>{lang==="fr" ? "formulaire de contact" : "contact form"}</h2>
+                    <h2 ref={headerRef}>{lang==="fr" ? "Formulaire de contact" : "Contact form"}</h2>
                     <button className="close-btn" onClick={()=>toggleOff()}>X</button>
 
                     <form autoComplete="off" className="contact-form">
@@ -65,6 +136,7 @@ function ContactForm({toggle, toggleOff, lang}){
                             <div className="form-elem">
                                 <label htmlFor="form-fname">{lang==="en"?"First name":"Prénom"}</label>
                                 <input type="text" name="fname" id="form-fname" 
+                                        ref={fnameRef}
                                         placeholder={lang==="en"?"Required":"Requis"}
                                         value={fname}
                                         onInput={(e)=>setFname(e.target.value)} />
@@ -72,6 +144,7 @@ function ContactForm({toggle, toggleOff, lang}){
                             <div className="form-elem">
                                 <label htmlFor="form-lname">{lang==="en"?"Last name":"Nom"}</label>
                                 <input type="text" name="lname" id="form-lname" 
+                                    ref={lnameRef}
                                     placeholder={lang==="en"?"Required":"Requis"}
                                     value={lname}
                                     onInput={(e)=>setLname(e.target.value)} />
@@ -88,6 +161,7 @@ function ContactForm({toggle, toggleOff, lang}){
                             <div className="form-elem">
                                 <label htmlFor="form-email">{lang==="en"?"Email address":"Adresse email"}</label>
                                 <input type="email" name="email" id="form-email"
+                                       ref={emailRef}
                                        placeholder={lang==="en"?"Required":"Requis"} 
                                        value={email}
                                        onInput={(e)=>setEmail(e.target.value)} />
@@ -99,6 +173,7 @@ function ContactForm({toggle, toggleOff, lang}){
                         <section className="form-row">
                             <div className="form-elem form-subject">
                                 <input id="subject" name="subject" type="text" 
+                                    ref={subjectRef}
                                     placeholder={lang==="en"?"Required":"Requis"}
                                     value={subject}
                                     onInput={(e)=>setSubject(e.target.value)} />
@@ -109,12 +184,18 @@ function ContactForm({toggle, toggleOff, lang}){
                         </section>
                         <section className="form-row">
                             <div className="form-elem form-msg">
-                                <textarea id="form-message" placeholder={lang==="en"?"Required":"Requis"}
+                                <textarea id="form-message" 
+                                        ref={msgRef}
+                                        placeholder={lang==="en"?"Required":"Requis"}
                                         value={msg}
                                         onInput={(e)=>setMsg(e.target.value)}></textarea>
                             </div>
+                       </section>
+                       <section className="form-row">
                             <div className="form-elem form-submit">
-                                <button type="submit" onClick={handleSubmit}>{lang==="en"?"Send": "Envoyer"}</button>
+                                <button type="submit" className='submit-button' onClick={handleSubmit}>
+                                    {lang==="en"?"Send":"Envoyer"}
+                                </button> 
                             </div>
                        </section>
                     </form>
